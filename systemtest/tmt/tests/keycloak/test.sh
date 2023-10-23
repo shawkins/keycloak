15e5ed9fc9611042ada4f1f33340eca51bcce130
Steve Hawkins <shawkins@redhat.com>
2023-10-23 10:52:47 -0400

    testing the tests

--- systemtest/tmt/tests/keycloak/test.sh
+++ systemtest/tmt/tests/keycloak/test.sh
@@ -2,23 +2,11 @@
 
 # Move to root folder of keycloak
 cd ../../../../
-
-#run tests
-if [[ ${IP_FAMILY} == "ipv4" || ${IP_FAMILY} == "dual" ]]; then
-    DOCKER_REGISTRY=$(hostname --ip-address | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | awk '$1 != "127.0.0.1" { print $1 }' | head -1)
-elif [[ ${IP_FAMILY} == "ipv6" ]]; then
-    DOCKER_REGISTRY="myregistry.local"
-fi
-
-export DOCKER_REGISTRY="$DOCKER_REGISTRY:5001"
-echo "Using container registry:$DOCKER_REGISTRY"
    
 ./mvnw install --batch-mode -Poperator -pl :keycloak-operator -am \
     -Dquarkus.kubernetes.image-pull-policy=IfNotPresent \
-    -Doperator.keycloak.image=keycloak:$DOCKER_TAG \
-    -Dquarkus.container-image.group=$DOCKER_ORG
-    -Dquarkus.kubernetes.env.vars.operator-keycloak-image-pull-policy=Never \
-    -Dtest.operator.custom.image=custom-keycloak:$DOCKER_TAG \
-    -Dquarkus.container-image.registry=$DOCKER_REGISTRY
+    -Doperator.keycloak.image=${KEYCLOAK_IMAGE} \
+    -Dquarkus.kubernetes.env.vars.operator-keycloak-image-pull-policy=IfNotPresent \
+    -Dquarkus.container-image.image=${KEYCLOAK_OPERATOR_IMAGE}
     --no-transfer-progress -Dtest.operator.deployment=remote
  
