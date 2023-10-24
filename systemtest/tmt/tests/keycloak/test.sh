b18c46318680a984930d64d8e6db26be7f1a441c
Steve Hawkins <shawkins@redhat.com>
2023-10-24 13:24:44 -0400

    adds openshift testing based upon testing-farm
    closes #21855

---
+++ systemtest/tmt/tests/keycloak/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -eux
+
+# Move to root folder of keycloak
+cd ../../../../
+   
+./mvnw install --batch-mode -Poperator -pl :keycloak-operator -am \
+    -Dquarkus.kubernetes.image-pull-policy=IfNotPresent \
+    -Doperator.keycloak.image=${KEYCLOAK_IMAGE} \
+    -Dquarkus.kubernetes.env.vars.operator-keycloak-image-pull-policy=IfNotPresent \
+    -Dquarkus.container-image.image=${KEYCLOAK_OPERATOR_IMAGE} \
+    -Dtest.operator.custom.image=${CUSTOM_IMAGE} \
+    --no-transfer-progress -Dtest.operator.deployment=remote
+ 
+ # TODO: olm tests
